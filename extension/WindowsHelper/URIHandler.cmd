8caf6bf6ccb11c86a1a32f0cd271f7cacf0f492d
John Riggles <jriggles@icloud.com>
2024-04-07 12:32:10 -0400

    Update URI handler helpers for Windows

--- extension/WindowsHelper/URIHandler.cmd
+++ extension/WindowsHelper/URIHandler.cmd
@@ -1,10 +1,22 @@
-REM add Aseprite to PATH for CLI access
-REM standalone Aseprite path
-SET "PATH=C:\Program Files\Aseprite;%PATH%"
-REM Steam Aseprite path
-SET "PATH=C:\Program Files (x86)\Steam\steamapps\common\Aseprite;%PATH%"
+@echo off
+SETLOCAL enableDelayedExpansion
+
+REM Initialize variable to store Aseprite executable path
+set "ASEPRITE_EXECUTABLE="
+
+REM Loop through all logical disk drives to find Aseprite.exe (needed for CLI access)
+for /f "skip=1 delims=" %%d in ('wmic logicaldisk get deviceid') do (
+    for /f %%e in ('where /r %%d "Aseprite.exe" 2^>nul') do (
+        if exist "%%e" (
+            set "ASEPRITE_EXECUTABLE=%%e"
+            REM Break the loop if Aseprite executable is found
+            goto :EndLoop
+        )
+    )
+)
+
+:EndLoop
 
 REM run lospec-palette-importer helper "lpihelper.lua", pass in url slug as app.params value
-REM try running from the standalone scripts dir first, then try the steam version if that fails
-aseprite.exe --script-param fromURI=%1 --script "C:\Users\%USERNAME%\AppData\Roaming\Aseprite\extensions\lospec-palette-importer\lpihelper.lua" || aseprite.exe --script-param fromURI=%1 --script "C:\Program Files (x86)\Steam\steamapps\common\Aseprite\data\extensions\lospec-palette-importer\lpihelper.lua" && EXIT
-REM %1 should is passed in by the URI and should be "lospec-palette://[palette slug]"
+REM %1 is the URI and should be "lospec-palette://[palette slug]"
+%ASEPRITE_EXECUTABLE% --script-param fromURI=%1 --script "%appdata%\Aseprite\extensions\lospec-palette-importer\lpihelper.lua"
