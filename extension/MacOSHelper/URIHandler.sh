a68a4b5a93151e2fd933bc1ad06a64d5c1104343
John Riggles <jriggles@icloud.com>
2024-04-06 21:43:44 -0400

    updat Mac OS URIHandler script
    Aseprite now gets focus on import automatically

--- extension/MacOSHelper/URIHandler.sh
+++ extension/MacOSHelper/URIHandler.sh
@@ -9,5 +9,9 @@ export PATH="/Users/$USER/Library/Application Support/Steam/steamapps/common/Ase
 
 # run lospec-palette-importer helper "lpihelper.lua", pass in url slug as app.params value
 # try running from the standalone scripts dir first, then try the steam version if that fails
-aseprite --script-param fromURI=$1 --script "/Users/$USER/Library/Application Support/Aseprite/extensions/lospec-palette-importer/lpihelper.lua" || aseprite --script-param fromURI=$1 --script "/Users/$USER/Library/Application Support/Steam/steamapps/common/Aseprite/data/extensions/lospec-palette-importer/lpihelper.lua" && exit
+aseprite --script-param fromURI=$1 --script "/Users/$USER/Library/Application Support/Aseprite/extensions/lospec-palette-importer/lpihelper.lua" ||
+aseprite --script-param fromURI=$1 --script "/Users/$USER/Library/Application Support/Steam/steamapps/common/Aseprite/data/extensions/lospec-palette-importer/lpihelper.lua" &
+# bring Aseprite to the front (requires "Privacy & Security > Automation" permission)
+osascript -e 'tell application "System Events" to set frontmost of process "Aseprite" to true'
+exit
 # $1 is passed in by the URI and should be "lospec-palette://[palette slug]"
